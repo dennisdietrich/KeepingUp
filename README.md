dfe16b9b6dcbe9e95c8beca9120829fb70d774be
Dennis "D.C." Dietrich <44212152+dennisdietrich@users.noreply.github.com>
2025-11-07 01:05:36 +0100

    First complete draft of slide deck (#1)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# KeepingUp
-Slides and samples for "Keeping up with Rachel Appel and the Commodore"
+## Presentation title
+Keeping up with the Commodore: 1980s performance optimization lessons still applicable today
+
+## Description
+Imaging finding an intriguing, 40-year-old book with type-in programs in BASIC for the legendary Commodore 64.
+And as you test the programs you just entered one numbered line at a time, something you hadn’t done since the early 1990s, you find that one spends over 15 seconds to draw a chess board.
+Another spends over a minute just to zero out 19 kB of RAM.
+Some feel sluggish and are slow to respond.
+Surely there’s got to be a better way, even when all you have are 8 bits at a time.
+So you start looking for ways to make things faster, but in the true spirit of retro computing, using only techniques available on actual hardware 40 years ago.
+In this talk we'll look at what makes Commodore BASIC so painfully slow, a few ways to deal with it, and how the techniques I used are still relevant today.
+
+--------------------------------------------------------------------------------
+
+### Disclaimer
+The content of this repo is provided "AS IS" with no warranties, and confers no rights.
---
+++ src/drawboard.asm
@@ -0,0 +1,85 @@
+*=$C000                ; Start of upper RAM area
+
+    color    = $02
+    colorram = $fb
+    pos      = $fd
+    counters = $61
+
+    lda #$00           ; Initialize pos to 1024
+    sta pos
+    lda #$04
+    sta pos + 1
+
+    lda #$03           ; Initialize color to 3 (cyan)
+    sta color
+
+    ldx #$01           ; Start of rows draw loop
+
+rowsloop
+
+    stx counters       ; Start lines draw loop
+    ldx #$01
+
+linesloop
+
+    stx counters + 1   ; Start squares draw loop
+    ldx #$01
+
+    ldy #$00           ; Index into screen/color RAM of current line
+
+    clc
+    lda pos            ; Calculate color RAM address for current line
+    sta colorram
+    lda pos + 1
+    adc #$d4
+    sta colorram + 1
+
+squaresloop
+
+    stx counters + 2   ; Start square draw loop
+    ldx #$01
+
+squareloop
+
+    lda #$a0           ; Write space to screen
+    sta (pos),y
+    lda color          ; Setting color
+    sta (colorram),y
+    iny;
+
+    inx                ; End of square draw loop
+    cpx #$05
+    bne squareloop
+    ldx counters + 2
+
+    lda color          ; Switch square color
+    eor #$03
+    sta color
+
+    inx                ; End of squares draw loop
+    cpx #$09
+    bne squaresloop
+    ldx counters + 1
+
+    clc                ; Increments value of pos by 40
+    lda pos            ; Adding 40 to low byte
+    adc #$28
+    sta pos
+    lda pos + 1        ; Adding carry to high byte
+    adc #$00
+    sta pos + 1
+
+    inx                ; End of lines draw loop
+    cpx #$04
+    bne linesloop
+    ldx counters
+
+    lda color          ; Switch square color
+    eor #$03
+    sta color
+
+    inx                ; End of rows draw loop
+    cpx #$09
+    bne rowsloop
+
+    rts
